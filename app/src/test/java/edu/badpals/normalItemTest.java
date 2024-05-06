4bba6e09f282244d1984585d089847d3e95d47bf
joel <joelfigueiridomolares@gmail.com>
2024-05-06 09:42:53 +0200

    feat(normalItemTest):"Creacion test normal item"

--- app/src/test/java/edu/badpals/normalItemTest.java
+++ app/src/test/java/edu/badpals/normalItemTest.java
@@ -1,5 +1,6 @@
 package edu.badpals;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.Arguments;
@@ -7,21 +8,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.stream.Stream;
 
-public class NormalItemTest {
+public class normalItemTest {
 
     @ParameterizedTest
     @MethodSource("sellInValues")
     public void testSellIn(int initialSellIn, int expectedSellIn, int quality) {
-        Item item = new Item("Dexterity Vest", initialSellIn, quality);
+        normalItem item = new normalItem("Dexterity Vest", initialSellIn, quality);
         item.update_sellIn();
         assertEquals(expectedSellIn, item.getSellIn());
     }
 
     private static Stream<Arguments> sellInValues() {
         return Stream.of(
-            Arguments.of(10, 8, 20),
+            Arguments.of(10, 9, 20),
             Arguments.of(-1, -2, 9),
             Arguments.of(0, -1, 10)
         );
+    }
+
+    @Test 
+    public void testQuality1() {
+        normalItem item = new normalItem("Dexterity Vest", 10, 20);
+        item.update_quality();
+        assertEquals(19, item.getQuality());
+    }
+
+    @Test 
+    public void testQuality2() {
+        normalItem item = new normalItem("Dexterity Vest", -1, 10);
+        item.update_quality();
+        assertEquals(8, item.getQuality());
+    }
+
+    @Test
+    public void testQuality3() {
+        normalItem item = new normalItem("Dexterity Vest", 0, 10);
+        item.update_quality();
+        assertEquals(8, item.getQuality());
+    }
+
+    @Test
+    public void testQuality4() {
+        normalItem item = new normalItem("Dexterity Vest", 1, 10);
+        item.update_quality();
+        assertEquals(9, item.getQuality());
     }
 }
