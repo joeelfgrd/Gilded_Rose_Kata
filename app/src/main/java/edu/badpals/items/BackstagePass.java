393dc32f28f234e8cafed077faa9776e949850ed
joel <joelfigueiridomolares@gmail.com>
2024-05-06 10:13:25 +0200

    feat(BackstagePass):"clase Backstagepass y test"

---
+++ app/src/main/java/edu/badpals/items/BackstagePass.java
@@ -0,0 +1,36 @@
+package edu.badpals.items;
+
+import edu.badpals.Item;
+
+public class BackstagePass implements Updateable {
+    private final Item item;
+    
+
+    public BackstagePass(String name, int sellIn, int quality) {
+        this.item =  new Item(name, sellIn, quality);
+    }
+
+    public void update_sellIn() {
+        this.item.sellIn -= 1;
+
+    }
+    public int getSellIn() {
+        return this.item.sellIn;
+    }
+    public int getQuality() {
+        return this.item.quality;
+    } public void update_quality() {
+        if (getSellIn() > 10) {
+            this.item.quality += 1;
+            return;}
+        else if(getSellIn() > 5) {
+            this.item.quality += 2;
+            return;}
+        else if(getSellIn() >= 0) {
+            this.item.quality += 3;
+            return;
+        }
+            this.item.quality = 0;
+
+        }
+}
