c5a4e76695eb4516dcdd68deccc0c9d85d4bddac
joel <joelfigueiridomolares@gmail.com>
2024-05-07 12:48:31 +0200

    feat(ItemTest.java)Creacion caso test para item

---
+++ app/src/test/java/edu/badpals/ItemTest.java
@@ -0,0 +1,16 @@
+package edu.badpals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ItemTest {
+    @Test
+    public void crearItem() {
+
+        Item item = new Item("+5 Dexterity Vest", 10, 20);
+
+        assertEquals("+5 Dexterity Vest", item.name);
+        assertEquals(10, item.sellIn, 0);
+        assertEquals(20, item.quality, 0);
+        System.out.println(item);
+    }
+}
